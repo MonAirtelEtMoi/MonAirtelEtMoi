35633d8ef8c06fb19370ced7cd761d437c0fb43e
drcdevfest <devfest15drc@gmail.com>
2015-12-29 14:57:50 +0200

    Correction bug sur TestFirebaseActivity.java

--- app/src/main/java/lab/synah/cd/monairteletmoi/gui/TestFirebaseActivity.java
+++ app/src/main/java/lab/synah/cd/monairteletmoi/gui/TestFirebaseActivity.java
@@ -1,11 +1,7 @@
 package lab.synah.cd.monairteletmoi.gui;
 
 import android.app.ListActivity;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,27 +12,19 @@ import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.firebase.ui.FirebaseListAdapter;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import lab.synah.cd.monairteletmoi.R;
 import lab.synah.cd.monairteletmoi.model.Ussd;
-import lab.synah.cd.monairteletmoi.utils.Utils;
 
-// import android.support.v7.app.AppCompatActivity;
 
 public class TestFirebaseActivity extends ListActivity {
 
     private Firebase mFireBaseRef; //Firebase reference
     FirebaseListAdapter<Ussd> mListAdapter; //To be used when retreiving data in the list
     public ListView mainListView;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-    Toolbar toolbar;
+
+
+   // Toolbar toolbar;
 
 
     @Override
@@ -47,16 +35,12 @@ public class TestFirebaseActivity extends ListActivity {
 
         Firebase.setAndroidContext(this);
 
-        mFireBaseRef = new Firebase(Utils.BACKEND_URL);
-
-
+        mFireBaseRef = new Firebase("https://MonAirtelEtMoi.firebaseio.com");
 
         /*
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        */
-
 
         TypedValue typedValueColorPrimaryDark = new TypedValue();
         TestFirebaseActivity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValueColorPrimaryDark, true);
@@ -64,8 +48,8 @@ public class TestFirebaseActivity extends ListActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(colorPrimaryDark);
         }
-
-        mainListView = (ListView) findViewById(R.id.list);
+        */
+        //mainListView = (ListView) findViewById(R.id.list);
 
         final EditText textEdit = (EditText) this.findViewById(R.id.text_edit);
         Button sendButton = (Button) this.findViewById(R.id.send_button);
@@ -100,9 +84,6 @@ public class TestFirebaseActivity extends ListActivity {
       // mainListView.setAdapter(mListAdapter);
 
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -134,43 +115,5 @@ public class TestFirebaseActivity extends ListActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "TestFirebase Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://lab.synah.cd.monairteletmoi.gui/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "TestFirebase Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://lab.synah.cd.monairteletmoi.gui/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
