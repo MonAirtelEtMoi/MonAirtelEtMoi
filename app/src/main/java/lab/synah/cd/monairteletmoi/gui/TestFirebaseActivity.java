7f6d9576dc08554c15eb255847acf1351095df46
drcdevfest <devfest15drc@gmail.com>
2015-12-29 11:50:22 +0200

    Fix Firebase access bug, change app toolbar color et fix other bugs

--- app/src/main/java/lab/synah/cd/monairteletmoi/gui/TestFirebaseActivity.java
+++ app/src/main/java/lab/synah/cd/monairteletmoi/gui/TestFirebaseActivity.java
@@ -1,10 +1,9 @@
 package lab.synah.cd.monairteletmoi.gui;
 
-import android.os.Build;
+import android.app.ListActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,15 +14,23 @@ import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.firebase.ui.FirebaseListAdapter;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import lab.synah.cd.monairteletmoi.R;
 import lab.synah.cd.monairteletmoi.model.Ussd;
 
-public class TestFirebaseActivity extends AppCompatActivity {
+public class TestFirebaseActivity extends ListActivity {
 
     private Firebase mFireBaseRef; //Firebase reference
     FirebaseListAdapter<Ussd> mListAdapter; //To be used when retreiving data in the list
     public ListView mainListView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
     Toolbar toolbar;
 
     @Override
@@ -33,9 +40,9 @@ public class TestFirebaseActivity extends AppCompatActivity {
         setContentView(R.layout.test_activity);
 
         Firebase.setAndroidContext(this);
-        mFireBaseRef=new Firebase("https://MonAirtelEtMoi.firebaseio.com");
+        mFireBaseRef = new Firebase("https://MonAirtelEtMoi.firebaseio.com");
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -44,7 +51,7 @@ public class TestFirebaseActivity extends AppCompatActivity {
         final int colorPrimaryDark = typedValueColorPrimaryDark.data;
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(colorPrimaryDark);
-        }
+        }*/
 
         final EditText textEdit = (EditText) this.findViewById(R.id.text_edit);
         Button sendButton = (Button) this.findViewById(R.id.send_button);
@@ -52,7 +59,7 @@ public class TestFirebaseActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text=textEdit.getText().toString();
+                String text = textEdit.getText().toString();
                 /*
                 // This method is used in case we have not a class model
 
@@ -62,26 +69,30 @@ public class TestFirebaseActivity extends AppCompatActivity {
 
                 */
 
-                Ussd codeussd=new Ussd("Code Ussd",text);
+                Ussd codeussd = new Ussd("Code Ussd", text);
                 mFireBaseRef.push().setValue(codeussd);
                 textEdit.setText((""));
             }
         });
 
-        mListAdapter=new FirebaseListAdapter<Ussd>(this,Ussd.class, android.R.layout.two_line_list_item,mFireBaseRef) {
+        mListAdapter = new FirebaseListAdapter<Ussd>(this, Ussd.class, android.R.layout.two_line_list_item, mFireBaseRef) {
             @Override
             protected void populateView(View v, Ussd model) {
-                ((TextView)v.findViewById(android.R.id.text1)).setText(model.getName());
-                ((TextView)v.findViewById(android.R.id.text2)).setText(model.getText());
+                ((TextView) v.findViewById(android.R.id.text1)).setText(model.getName());
+                ((TextView) v.findViewById(android.R.id.text2)).setText(model.getText());
             }
         };
-        mainListView.setAdapter(mListAdapter);
+        setListAdapter(mListAdapter);
+       // mainListView.setAdapter(mListAdapter);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         mListAdapter.cleanup();
     }
@@ -107,5 +118,45 @@ public class TestFirebaseActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TestFirebase Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://lab.synah.cd.monairteletmoi.gui/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TestFirebase Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://lab.synah.cd.monairteletmoi.gui/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
